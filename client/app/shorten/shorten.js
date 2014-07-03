eee179b050a9a97ac1ad8fccc43f63f4ae1683eb
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-07-02 19:45:39 -0700

    Implement addLink method and loading property on $scope

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,9 +3,12 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
+  $scope.loading = false;
   $scope.addLink = function(){
+    $scope.loading = true;
     Links.addLink()
     .then(function(link){
+      $scope.loading = false;
       $scope.link = link;
     })
     .catch(function(error){
--- server/links/linkRoutes.js
+++ server/links/linkRoutes.js
@@ -14,5 +14,6 @@ module.exports = function (app) {
     .post(linksController.newLink);
 
   app.get('/:code', linksController.navToLink);
+  app.post('/shorten', linksController.newLink);
 
 };
